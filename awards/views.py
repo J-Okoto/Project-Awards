c607a35dd7a61a2959c0f12f9409859661c3a338
J-Okoto <okotojr@gmail.com>
2020-10-26 20:22:12 +0300

    added search function and display html

--- awards/views.py
+++ awards/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
-
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Post,Profile,Rating
+from .forms import PostForm,ProfileForm,RatingForm
+import datetime as dt
+from django.http import JsonResponse
+import json
+from django.contrib.auth.models import User
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def welcome(request):
     return render(request, 'index.html')
+
+def search_results(request):
+    current_user = request.user
+    profile =Profile.objects.get(username=current_user)
+    if 'project' in request.GET and request.GET["project"]:
+        search_term = request.GET.get("project")
+        searched_projects = Project.search_project(search_term)
+        message=f"{search_term}"
+
+        print(searched_projects)
+
+        return render(request,'search.html',{"message":message,"projects":searched_projects,"profile":profile})
+
+    else:
+        message="You haven't searched for any term"
+        return render(request,'search.html',{"message":message})
