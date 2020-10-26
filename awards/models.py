229f828583a704fe48f878692928bc886a45d124
J-Okoto <okotojr@gmail.com>
2020-10-26 17:32:55 +0300

    added models to admin page

--- awards/models.py
+++ awards/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
@@ -18,36 +19,36 @@ class Profile(models.Model):
     def __str__(self):
         return self.user_name
 
-    class Post(models.Model):
-    uploaded_by = models.ForeignKey(User, null=True, related_name='posts')
-    name = models.CharField(max_length=200, null=True)
-    landing_image = models.ImageField(upload_to='site-images/', null=True)
-    description = models.TextField(blank=True)
-    site_link = models.CharField(max_length=200, null=True)
-    post_date = models.DateTimeField(auto_now_add=True)
+class Post(models.Model):
+        uploaded_by = models.ForeignKey(User, null=True, related_name='posts')
+        name = models.CharField(max_length=200, null=True)
+        landing_image = models.ImageField(upload_to='site-images/', null=True)
+        description = models.TextField(blank=True)
+        site_link = models.CharField(max_length=200, null=True)
+        post_date = models.DateTimeField(auto_now_add=True)
 
-    @classmethod
-    def all_posts(cls):
-        all_posts = cls.objects.all()
-        return all_posts
+        @classmethod
+        def all_posts(cls):
+            all_posts = cls.objects.all()
+            return all_posts
 
-    @classmethod
-    def filter_by_search_term(cls, search_term):
-        return cls.objects.filter(description__icontains=search_term)
+        @classmethod
+        def filter_by_search_term(cls, search_term):
+            return cls.objects.filter(description__icontains=search_term)
 
-    def get_user_profile(self, post):
-        posts = self.objects.filter(uploaded_by=post.uploaded_by)
-        return posts
+        def get_user_profile(self, post):
+            posts = self.objects.filter(uploaded_by=post.uploaded_by)
+            return posts
 
-    def get_one_post(self, post_id):
-        return self.objects.get(pk=post_id)
+        def get_one_post(self, post_id):
+            return self.objects.get(pk=post_id)
 
-    def save_post(self, user):
-        self.uploaded_by = user
-        self.save()
+        def save_post(self, user):
+            self.uploaded_by = user
+            self.save()
 
-    def __str__(self):
-        return self.name
+        def __str__(self):
+            return self.name
 
 
 class Rating(models.Model):
