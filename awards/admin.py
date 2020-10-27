17a42f2d8f220db50024a53d5ae2530ef77815f2
J-Okoto <okotojr@gmail.com>
2020-10-27 11:47:45 +0300

    added new models to admin page

--- awards/admin.py
+++ awards/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Profile,Project,countries,Rating
+
+
+
+
+# Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Project)
+admin.site.register(countries)
+admin.site.register(Rating)
 
--- awards/tests.py
+++ awards/tests.py
@@ -16,8 +16,8 @@ class TestUser (TestCase):
         User.objects.all().delete()
 
     def test_instance(self):
-        self.assertEqu
-        al(self.joe.username, 'joe')
+        self.assertEqual
+        (self.joe.username, 'joe')
         self.assertEqual(self.joe.password, '6868uy')
         self.assertTrue(isinstance(self.joe, User))
 
@@ -26,47 +26,47 @@ class TestUserProfile (TestCase):
 
         self.joe = User(username='joe', password='6868uy')
         self.joe.save()
-        self.joey = Profile(user=self.joe, first_name='Joel',
-                                  user_name='joey', bio='cool', email='joe@gmail.com',number= '0722222222')
+        self.joey = Profile(user=self.joe, name='Joel',
+                                  user_name='joey', description='cool', email='joe@gmail.com',country= 'kenya')
         self.joey.save_profile(self.joe)
 
     def tearDown(self):
         Profile.objects.all().delete()
 
     def test_instance(self):
-        self.assertEqual(self.joey.first_name, 'Joel')
+        self.assertEqual(self.joey.name, 'Joel')
         self.assertEqual(self.joey.user_name, 'joey')
-        self.assertEqual(self.joey.bio, 'i cool')
+        self.assertEqual(self.joey.description, 'i cool')
         self.assertEqual(self.joey.email, 'joe@gmail.com')
-        self.assertEqual(self.joey.number, '0722222222')
+        self.assertEqual(self.joey.country, 'kenya')
         self.assertTrue(isinstance(self.joey, Profile))
 
     def test_save_profile(self):
         self.joey.save_profile(self.joe)
         self.assertTrue(len(Profile.objects.all()) > 0)
 
-class TestPost (TestCase):
+class TestProject (TestCase):
     def setUp(self):
         self.joe = User(username='joe', password='6868uy')
         self.joe.save()
-        self.forest = Post(uploaded_by=self.joe, landing_image='test.jpg',
+        self.forest = Project(uploaded_by=self.joe, landing_page='test.jpg',
                               description='lovely', post_date=datetime.utcnow())
-        self.forest.save_post(self.joe)
+        self.forest.save_project(self.joe)
 
     def tearDown(self):
-        Post.objects.all().delete()
+        Project.objects.all().delete()
 
     def test_instance(self):
         self.assertEqual(self.forest.uploaded_by, self.joe)
-        self.assertEqual(self.forest.landing_image, 'test.jpg')
+        self.assertEqual(self.forest.landing_page, 'test.jpg')
         self.assertEqual(self.forest.description, 'lovely')
-        self.assertTrue(isinstance(self.forest, Post))
+        self.assertTrue(isinstance(self.forest, Project))
 
-    def test_all_posts(self):
-        self.assertTrue(len(Post.objects.all()) > 0)
+    def test_all_projects(self):
+        self.assertTrue(len(Project.objects.all()) > 0)
 
-    def test_user_posts(self):
-        self.assertTrue(len(Post.get_user_profile(self.joe)) > 0)
+    def test_user_projects(self):
+        self.assertTrue(len(Project.get_user_profile(self.joe)) > 0)
 
 
 
